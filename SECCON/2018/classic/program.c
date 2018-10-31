3dda31a5aade9d8976708d05778599cfe575c412
Sajjad Arshad <msarshadir@gmail.com>
2018-10-31 11:56:25 -0400

    adding source

--- SECCON/2018/classic/program.c
+++ SECCON/2018/classic/program.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int64_t init()
+{
+  setbuf(stdout, 0LL);
+  setbuf(stderr, 0LL);
+  return 0LL;
+}
 
 int main()
 {
-    char buf[0x40];
-    puts("Classic Pwnable Challenge");
-    printf("Local Buffer >> ");
-    gets(buf);
-    puts("Have a nice pwn!!");
-    return 0;
+  char buf[0x40];
+
+  puts("Classic Pwnable Challenge");
+  printf("Local Buffer >> ");
+  gets(buf);
+  puts("Have a nice pwn!!");
+  return 0;
 }
