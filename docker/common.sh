a16a08e8dc1b3441e9b92405f1a7767c8dc87abb
Sajjad Arshad <arshad@ccs.neu.edu>
2019-04-05 08:22:44 -0400

    using amd64 for arm

--- docker/common.sh
+++ docker/common.sh
@@ -2,7 +2,7 @@
 
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <Ubuntu Release>" >&2
-    echo "       Ubuntu Release can be 16.04, 17.10, 18.04, 18.10, ..." >&2
+    echo "       Ubuntu Release can be any supported version of the Ubuntu such as 16.04, 17.10, 18.04, 18.10, ..." >&2
     exit 1
 fi
 
